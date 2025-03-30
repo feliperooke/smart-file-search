bb48c1f397a389fddb12795d01c55fcb5eb7a4c9
feliperooke <felipe@rooke.com.br>
2025-03-30 14:24:17 -0300

    Add Terraform configuration for S3 bucket and Lambda function integration
    - Introduced .env.sample for environment variable management.
    - Updated main.tf to use variables for Lambda function name and added S3 bucket resource with versioning.
    - Implemented S3 bucket policy and IAM role policy for Lambda access to S3.
    - Added output for S3 bucket name in outputs.tf.
    - Defined new variables for S3 bucket name and Lambda function name in variables.tf.

--- backend/.aws/terraform/outputs.tf
+++ backend/.aws/terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "invoke_url" {
   value = "${aws_apigatewayv2_api.api.api_endpoint}/${aws_apigatewayv2_stage.dev.name}/"
 }
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.file_storage.id
+}
--- backend/.aws/terraform/variables.tf
+++ backend/.aws/terraform/variables.tf
@@ -1,3 +1,14 @@
 variable "aws_region" {
-  default = "us-east-1"
+  description = "AWS region"
+  type        = string
+}
+
+variable "s3_bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
+
+variable "lambda_function_name" {
+  description = "Name of the Lambda function"
+  type        = string
 }
